036f85d2c4ec6159aff49b8ebcb8e113478e8dcd
Sunset-Kim <77092632+Sunset-Kim@users.noreply.github.com>
2021-12-10 01:32:00 +0900

    🚧 💄  ui foundation : button, badge, link

---
+++ src/components/Badges.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled, { StyledComponent, StyledComponentBase, StyledComponentProps, StyledProps } from "styled-components";
+interface Container {
+  bgColor: string
+  textColor: string
+}
+
+
+const BadgeContainer = styled.span<Container>`
+display: inline-flex;
+justify-content: center;
+align-items: center;
+padding: 4px 16px;
+border-radius: 100px;
+background: ${props => props.bgColor};
+color: ${props => props.textColor};
+`;
+
+interface BasicBadgeProps {
+  name: string
+  bgColor: string
+  textColor: string
+}
+
+const BasicBadge: React.FC<BasicBadgeProps> = ({ name, bgColor, textColor }) => {
+  return (
+    <BadgeContainer bgColor={bgColor} textColor={textColor}>
+      {name}
+    </BadgeContainer>
+  )
+}
+
+const ReactBadge = () => {
+  return <BasicBadge name="React" bgColor="crimson" textColor="white" />
+}
+
+const HtmlBadge = () => {
+  return <BasicBadge name="Html" bgColor="orange" textColor="white" />
+}
+
+const JSBadge = () => {
+  return <BasicBadge name="JS" bgColor="gold" textColor="white" />
+}
+
+const JQBadge = () => {
+  return <BasicBadge name="JQuery" bgColor="dodgerblue" textColor="white" />
+}
+
+export { BasicBadge, ReactBadge, HtmlBadge, JSBadge, JQBadge }
