7f423c9e69c45b28ee954fe203595961acd03eb3
Sunset-Kim <77092632+Sunset-Kim@users.noreply.github.com>
2021-12-11 01:58:56 +0900

    💄  버튼 컴포넌트추가

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Buttons = () => {
+interface ButtonStyleProps {
+  bgColor?: string
+  textColor?: string
+}
+
+const Button = styled.button<ButtonStyleProps>`
+padding: 8px 24px;
+font-size: 14px;
+text-decoration: none;
+border-radius: 100px;
+background-color: ${props => props.bgColor ?? props.theme.color.backgorund};
+color: ${props => props.textColor ?? props.theme.color.text1};
+`
+const Link = styled.a<ButtonStyleProps>`
+padding: 8px 24px;
+font-size: 14px;
+text-decoration: none;
+border-radius: 100px;
+background-color: ${props => props.bgColor ?? props.theme.color.backgorund};
+color: ${props => props.textColor ?? props.theme.color.text1};
+`;
+
+interface BasicButtonProps {
+  name: string
+  bgColor?: string
+  textColor?: string
+}
+
+const BasicButton: React.FC<BasicButtonProps> = ({ name, bgColor, textColor }) => {
   return (
-    <div>
-
-    </div>
+    <Button bgColor={bgColor} textColor={textColor}>
+      {name}
+    </Button>
   )
 }
 
-export default Buttons
+interface LinkButtonProps extends BasicButtonProps {
+  url?: string
+}
+
+const LinkButton: React.FC<LinkButtonProps> = ({ name, bgColor, url }) => {
+  const href = url ?? 'javascript:;';
+  return (
+    <Link href={href} target={url ? "_blank" : ""} >
+      {name}
+    </Link>
+  )
+}
+
+export { BasicButton, LinkButton }
