9b9632cf05659bee36977c98676d4b1f14d4d42d
Sunset-kim <wood2002@naver.com>
2021-12-18 23:21:37 +0900

    💄  profile 탭 ui 변경 및 내용추가 (인사말 , contact, skill 추가)

--- src/screen/Profile.tsx
+++ src/screen/Profile.tsx
@@ -1,89 +1,124 @@
 import React from 'react'
-import styled from 'styled-components'
-// header
-const Title = styled.h2``;
+import styled, { useTheme } from 'styled-components'
+import Dialog from '../components/Dialog';
+import {Keywords, SkillBadge} from '../components/Badges'
+import { GithubBadge,MailBadge,BlogBadge } from '../components/Badges';
 
-// info
-const InfoContainer = styled.div`
-display: flex;
+// header
+const Title = styled.h1`
+font-family: 'Roboto';
+font-size: 16px;
+margin: 0;
 `;
 
-const InfoImg = styled.div`
+const SubTitle = styled.h2`
+font-family: 'Norwester';
+font-size: 60px;
+margin: 0;
+margin-bottom: 20px;
+`;
+
+const DialogContainer = styled(Dialog)`
+margin-bottom: 50px;
+`;
+
+// info
+const Section = styled.section`
+display: flex;
+padding: 24px 0;
+`;
+
+const Left = styled.div`
+flex-direction: column;
+flex: 1;
+margin-right: 24px;
+`;
+
+const Right  = styled.div`
 position: relative;
-flex: 2 1;
+flex: 1 1;
 margin: 0 auto;
 `;
 
 const ImageContainer = styled.div`
 width: 100%;
 height: 100%;
-border-radius: 80% 55% 45% 70% / 90% 60% 35% 60%;
-border: 1px solid gray;
+border-radius: 5px;
 overflow: hidden;
-
 img {
   object-fit: cover;
   width: 100%;
   height: 100%;
 }
 `;
-const InfoLeft = styled.div`
-flex-direction: column;
-flex: 1;
-margin-right: 24px;
 
+const SkillContainer = styled.div`
+display: flex;
+flex-wrap: wrap;
+padding: 24px;
+border-radius: 10px;
+background-color: ${props => props.theme.color.foreground};
+
+span {
+  margin: 5px;
+}
 `;
 
-// skill
-const SkillContainer = styled.div``;
+
 
 
 const Profile = () => {
+  const theme = useTheme();
   return (
     <>
-      <Title>
-        안녕하세요.<br /> 프론트엔드 개발자 김민우 입니다.
-      </Title>
-      <InfoContainer>
-        <InfoLeft>
-          <div>
-            <h3>
-              BIOGRAPHY
-            </h3>
-            <p>
+    {/* about */}
+      <Section>
+        <Left>
+          <Title>
+            Frontend Delveloper Portpolio
+          </Title>
 
-            </p>
-          </div>
-          <div>
-            <h3>
-              BIOGRAPHY
-            </h3>
-            <p>
+          <SubTitle>
+            About
+          </SubTitle>
 
-            </p>
-          </div>
-          <div>
-            <h3>
-              BIOGRAPHY
-            </h3>
-            <p>
-
-            </p>
-          </div>
-        </InfoLeft>
-        <InfoImg>
+          <DialogContainer title='Greeting' mb={20}>
+            {`안녕하세요.\n프론트엔드 개발자 김민우입니다.\n적당한 고난과 역경은 감사합니다`}
+          </DialogContainer>
+          
+          <DialogContainer title='Contact Me' mb={20}>
+            <GithubBadge url='#none' />
+            <BlogBadge url="#none" />
+            <MailBadge url="#none" />
+          </DialogContainer>
+        </Left>
+        <Right>
           <ImageContainer>
             <img src="/images/profile_img.jpg" alt="profile" />
           </ImageContainer>
-        </InfoImg>
+        </Right>
 
-      </InfoContainer>
-      <SkillContainer>
-        <Title>
-          Skill
-        </Title>
+      </Section>
 
-      </SkillContainer>
+      <Section>
+        <Left>
+          <SubTitle>
+            Skills
+          </SubTitle>
+        </Left>
+        <Right>
+          <SkillContainer>
+          {
+              ['html', 'css', 'jquery', 'javascript', 'typescript', 'react', 'react-native']
+              .map(item => <SkillBadge key={item} type={item as Keywords} bgColor={theme.color.backgorund} />)
+            }
+          </SkillContainer>
+            
+        </Right>
+
+      
+        
+      </Section>
     </>
   )
 }
