f789954126e290d81ee86025dc2122e79ef01fe9
Sunset-kim <wood2002@naver.com>
2021-12-18 23:19:10 +0900

    ♻️ 💄  poster ui 버전업 (title, desc, github, link, thumbnail, video)

--- src/components/Toy/Poster.tsx
+++ src/components/Toy/Poster.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
-import { LinkButton } from '../Buttons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faGithub} from '@fortawesome/free-brands-svg-icons'
+import LinkButton from '../LinkButton';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { project } from '../../data/projectData';
 
 const Container = styled.div`
 position: relative;
@@ -19,38 +23,126 @@ height: 100%;
 const Front = styled(ContainerChild)`
 display: flex;
 flex-direction: column;
-justify-content: flex-end;
-padding: 24px;
+justify-content: space-between;
+padding: 24px 12px;
 `;
 
-const Title = styled.h2`
-font-size: 20px;
-margin-bottom: 10px;
+const Info = styled.div`
+flex: 1;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
 `;
+const Header = styled.div``;
+
 const Desc = styled.p`
 font-size: 14px;
 line-height: 20px;
 `;
 
-const Back = styled(ContainerChild)`
+const LinkContainer = styled.div`
+align-self: flex-end;
 display: flex;
-flex-direction: column;
-justify-content: space-between;
-padding: 24px;
+justify-content: flex-end;
+margin-bottom: 10px;
 `;
 
-const Poster = () => {
+const Link = styled.a`
+display: inline-flex;
+justify-content: center;
+align-items: center;
+width: 30px;
+height: 30px;
+border-radius: 50%;
+background-color: wheat;
+`;
+
+const Title = styled.h2`
+font-family: 'Noto Sans KR';
+font-size: 20px;
+line-height: 28px;
+margin: 0;
+margin-bottom: 0.3em;
+`;
+
+const KeywordsContainer = styled.div``;
+
+const ImageContainer = styled.div`
+display: flex;
+align-items: flex-end;
+height: 50%;
+overflow: hidden;
+
+img {
+  width: 100%;
+  height: 100%;
+  object-position: top;
+  object-fit: cover;
+}
+`;
+
+const VideoContainer = styled(ImageContainer)`
+position: relative;
+
+video {
+  position: absolute;
+  top: 0;
+
+width: 100%;
+
+}
+`;
+
+const Poster:React.FC<project> = ({...props}) => {
   return (
     <Container>
-      <Back>
-
-      </Back>
       <Front>
-        <div>
-          <Title>제목</Title>
-          <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore expedita ex perspiciatis ut ab modi nobis? Vel consequatur at sed!</Desc>
-          <LinkButton name="link" />
-        </div>
+      <Info>
+        <Header>
+            <Title>{props.title}</Title>
+            <Desc>{props.desc}</Desc>
+            <KeywordsContainer>
+              {props.keywords.map(item => <span>{item}</span>)}
+            </KeywordsContainer>
+        </Header>
+        
+        <LinkContainer>
+        {
+          // github link
+          props.github &&
+          <LinkButton url={props.github} size={30}>
+            <FontAwesomeIcon icon={faGithub} size='1x'/>
+          </LinkButton>
+        }
+
+        {
+          // project links
+          props.url.length > 0 &&
+          props.url.map(item => <LinkButton key={item} url={item} size={30} >
+          <FontAwesomeIcon icon={faPaperPlane} size='1x'/>
+          </LinkButton>)
+        }
+        </LinkContainer>
+      </Info>
+
+      
+        {
+          props.thumbnail &&
+          <ImageContainer>
+            <img src={props.thumbnail} alt={props.title} />
+          </ImageContainer>
+        }
+
+      
+      {
+          props.video &&
+          <VideoContainer>
+            <video src={props.video} autoPlay muted loop/>
+          </VideoContainer>
+          
+        }
+    
+
       </Front>
 
 
