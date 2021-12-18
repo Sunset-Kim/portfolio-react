276e29560db5d0f070e1cc4aac5d22d610bbc9bf
Sunset-kim <wood2002@naver.com>
2021-12-18 23:19:44 +0900

    🍱  project, toy 데이터 추가

---
+++ src/data/projectData.ts
@@ -0,0 +1,73 @@
+export type keyword = 'react'|'react-native' | 'typescript' | 'javascript' | 'jquery' | 'html' | 'sass' | 'css';
+
+export interface project {
+  id: number;
+  title: string;
+  subtitle?: string;
+  desc: string;
+  thumbnail?: string;
+  url: string[];
+  github?: string
+  keywords: keyword[];
+  video?: string
+}
+
+const projectData: project[] = [
+  {
+    id: 20210705,
+    title: "국제백신연구소\n한국후원회\n웹사이트",
+    subtitle: "반응형페이지 크로스브라우징",
+    desc: "고정형 admin페이지와 반응형 사용자페이지 개발 및 크로스브라우징 전체. 프론트단 전체 개발",
+    thumbnail: "./images/project/ivi.png",
+    url: ["https://www.ivisupport.or.kr"],
+    keywords: ['html', 'jquery', 'sass']
+  },
+  {
+    id: 20210618,
+    title: "선한영향력가게",
+    subtitle: "반응형페이지 크로스브라우징",
+    desc: "고정형 admin페이지와 반응형 사용자페이지 개발 및 크로스브라우징 전체. 프론트단 전체 개발",
+    thumbnail: "./images/project/goodstore.png",
+    url: ["https://선한영향력가게.com"],
+    keywords: ['html', 'jquery', 'sass']
+  },
+  {
+    id: 20210628,
+    title: "네이버 해피빈",
+    subtitle: "적응형페이지 크로스브라우징",
+    desc: "네이버 해피빈 이벤트 페이지 다수 제작. 퍼블리싱 및 스크립트 작업",
+    thumbnail: "./images/project/happybean.png",
+    url: [
+      "https://campaign.happybean.naver.com/campaign/give/ecensus",
+      "https://campaign.happybean.naver.com/campaign/give/2021zeroenergybuilding",
+      "https://happybean.naver.com/campaign/etechhive",
+      "https://campaign.happybean.naver.com/campaign/give/iskwomancare",
+      "https://campaign.happybean.naver.com/campaign/give/dreamgream",
+      "https://campaign.happybean.naver.com/campaign/give/2021withibk",
+      "https://campaign.happybean.naver.com/campaign/give/2021pampersclub",
+      "https://happybean.naver.com/campaign/kcgp",
+      "https://happybean.naver.com/campaign/etechhive",
+    ],
+    keywords: ['html', 'jquery', 'sass']
+  },
+  {
+    id: 20210908,
+    title: "세이브더칠드런\n캠페인",
+    subtitle: "적응형페이지 크로스브라우징",
+    desc: "세이브더칠드런 pc용 스크롤 애니메이션 페이지 제작, 템플릿 페이지 제작 및 코드설계",
+    video: "./images/project/svc.mp4",
+    url: ["https://happybean.naver.com/campaign/etechhive"],
+    keywords: ['html', 'javascript', 'sass']
+  },
+  {
+    id: 20210701,
+    title: "기부플랫폼\n바스켓",
+    subtitle: "모바일웹",
+    desc: "유지보수, 레거시코드 정리, 크로스브라우징, 코드컨벤션, 스타일 모듈화 및 디자인 시스템 정리",
+    thumbnail: "./images/project/basket.png",
+    url: ["https://basket.fund/page/service/friends"],
+    keywords: ['html', 'sass', 'jquery']
+  },
+];
+
+export default projectData 
---
+++ src/data/toyData.ts
@@ -0,0 +1,68 @@
+import { project } from "./projectData";
+
+const toyData: project[] = [
+  {
+    id: 20202020,
+    title: "칼하트클론WIP 클론",
+    desc: "Sass로 webgird를 사용해 구현",
+    keywords: ['jquery','sass'],
+    thumbnail: "./images/toy/calhartt.png",
+    url: ["https://minwoobabu.cafe24.com/asset/14_project/index.html"],
+    github: "https://github.com/Sunset-Kim/SASS_Carhartt_clone"
+  },
+  {
+    id: 20202021,
+    title: "템플릿디자인 구현",
+    desc: "고정형 템플릿 디자인을 반응형 웹으로 개선",
+    keywords: ['jquery','sass'],
+    thumbnail: "./images/toy/template.png",
+    url: ["https://minwoobabu.cafe24.com/asset/11_project/index.html",],
+    github: "https://github.com/Sunset-Kim/Vue_templeate"
+  },
+  {
+    id: 20202022,
+    title: "라이카 반응형",
+    desc: "고정형 사이트를 반응형으로 개선",
+    keywords: ['jquery','css'],
+    thumbnail: "./images/toy/leica.png",
+    url: ["http://minwoobabu.cafe24.com/asset/11_project/index.html",],
+    github: "https://github.com/Sunset-Kim/leica-clone"
+  },
+  {
+    id: 20202022,
+    title: "원페이지 스크롤 사이트",
+    desc: "간단한 형식의 원페이지 스크롤 구현",
+    keywords: ['jquery','css'],
+    thumbnail: "./images/toy/onpage.png",
+    url: ["https://minwoobabu.cafe24.com/asset/02_project/index.html",],
+  },
+  {
+    id: 20202022,
+    title: "아이패드 모션페이지 제작",
+    desc: "아이패드 모션페이지와 유사한 인터렉션을 제작",
+    keywords: ['javascript','css'],
+    video: './images/toy/jsinteraction.mp4',
+    url: ["https://minwoobabu.cafe24.com/asset/13_project/index.html",],
+    github: 'https://github.com/Sunset-Kim/JS_interaction_page'
+  },
+  {
+    id: 20202022,
+    title: "Koovie",
+    desc: "react-native, movie API를 사용한 영화소개 app제작",
+    keywords: ['react-native','typescript'],
+    video: './images/toy/koovie.mp4',
+    url: [],
+    github: 'https://github.com/Sunset-Kim/Koovies'
+  },
+  {
+    id: 20202022,
+    title: "Kimflix",
+    desc: "react, movie API를 사용한 영화소개 web제작",
+    keywords: ['react-native','typescript'],
+    thumbnail: './images/toy/kimflix.png',
+    url: ['https://kimflix11.netlify.app/'],
+    github: 'https://github.com/Sunset-Kim/Koovies'
+  }
+];
+
+export default toyData 
