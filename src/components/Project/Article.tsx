6b07f5d3e018fd6baa35f5d4271e21ac001417a2
Sunset-kim <wood2002@naver.com>
2021-12-18 23:17:54 +0900

    ♻️ 💄  Article 스타일 버전업 (link, category, title desc, thumbnail, video)

--- src/components/Project/Article.tsx
+++ src/components/Project/Article.tsx
@@ -1,105 +1,146 @@
 import React from 'react'
 import styled from 'styled-components'
+import LinkButton from '../LinkButton';
+import Dialog from '../Dialog';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 const ProjectContainer = styled.div`
 position: relative;
-width: 100%;
-padding-top: 80%;
-border: 1px solid black;
+padding-top: 75%;
 `;
 
-const ProjectChild = styled.div`
+const ProjectContents = styled.div`
 position: absolute;
-top: 0;
-left: 0;
 width: 100%;
 height: 100%;
+left: 0;
+top: 0;
+display: flex;
+justify-content: space-between;
+padding: 25px 0;
 `;
 
-const ProjectInfo = styled(ProjectChild)`
+const Info = styled.div`
+margin-right: 32px;
+flex: 1;
+height: 400px;
 display: flex;
 flex-direction: column;
 justify-content: space-between;
-padding: 24px;
 `;
 
-const InfoHeader = styled.div``;
-const InfoBottom = styled.div``;
+const Header = styled.div``;
 
-const BadgeContainer = styled.div`
-margin-bottom: 24px;
+const Title = styled.h2`
+font-size: 28px;
+line-height: 38px;
+margin: 0;
+font-weight: 600;
+margin-bottom: 0.5em;
+`;
+
+const SubTitle = styled.h5`
+font-size: 14px;
+line-height: 18px;
+font-weight: 400;
+margin: 0;
+margin-bottom: 1em;
+
 span {
-  margin-right: 8px;
-  &:last-child {
-    margin-right: 0;
-  }
+  display: block;
+  margin-bottom: 0;
 }
 `;
 
-const Title = styled.h2``;
-const Desc = styled.p``;
-
-const ProjectBackground = styled(ProjectChild)`
-z-index: -1;
+const Desc = styled.p`
+font-size: 15px;
+line-height: 24px;
 `;
 
-const RectLeft = styled(ProjectChild) <{ deg: string }>`
-z-index: 1;
-background: ${props => props.theme.color.backgorund};
-transform: ${props => `skew(${props.deg}) translate3d(-50%,0,0)`};
+const LinkContainer = styled(Dialog)`
+display: flex;
+flex-wrap: wrap;
 `;
 
-const RectRight = styled(RectLeft) <{ deg: string }>`
-transform: ${props => `skew(${props.deg}) translate3d(80%,0,0)`};
-`;
-
-const Background = styled(ProjectChild)`
+const Img = styled.div`
+flex: 1;
+display: flex;
+align-items: flex-end;
 
 img {
   width: 100%;
   height: 100%;
   object-fit: cover;
+  object-position: left top;
 }
+`;
+
+const VideoContainer = styled(Img)`
+  video {
+    width: 100%;
+  }
 `;
 
 interface ArticleProps {
   title: string
+  subtitle?: string
   desc: string
-  img: string
-  direction?: 'right' | 'left'
-  badge: Array<() => JSX.Element>
+  thumbnail?: string
+  keywords: string[]
+  url?: string[]
+  video?: string
 }
 
-const Article: React.FC<ArticleProps> = ({ title, desc, img, badge, direction = 'right' }) => {
-
-  const deg = direction === 'right' ? '-20deg' : '20deg'
+const Article: React.FC<ArticleProps> = ({ title, subtitle, desc, thumbnail, keywords,url, video }) => {
+  
 
   return (
     <ProjectContainer>
-      <ProjectInfo>
-        <InfoHeader>
-          <BadgeContainer>
-            {badge.map(item => item())}
-          </BadgeContainer>
+      <ProjectContents>
+        <Info>
+          <Header>
+          {
+            subtitle &&
+            <SubTitle>
+              {subtitle.split(' ').map(item => <span>{item}</span>)}
+            </SubTitle>
+          }
           <Title>
             {title}
           </Title>
+
           <Desc>
             {desc}
           </Desc>
-        </InfoHeader>
-        <InfoBottom>
-          link
-        </InfoBottom>
-      </ProjectInfo>
-      <ProjectBackground>
-        <RectLeft deg={deg} />
-        <RectRight deg={deg} />
 
-        <Background>
-          <img src={img} alt="프로필" />
-        </Background>
-      </ProjectBackground>
+          </Header>
+          {
+            url &&
+            <LinkContainer title='Links' mb={0}>
+              {url.map(link => <LinkButton url={link} >
+                <FontAwesomeIcon icon={faPaperPlane} />
+              </LinkButton>
+              )}
+            </LinkContainer>
+          }
+        </Info>
+
+          {
+            thumbnail && 
+            <Img>
+              <img src={thumbnail} alt="프로필" />
+            </Img>
+          }
+
+          {
+            video && 
+            <VideoContainer>
+              <video src={video} autoPlay muted loop/>
+            </VideoContainer>
+          }
+       
+      </ProjectContents>
     </ProjectContainer>
   )
 }
