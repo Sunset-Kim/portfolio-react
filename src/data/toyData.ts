37e68d7b6eacebbd935b89e713eddb0c9f8a7381
Sunset-kim <wood2002@naver.com>
2021-12-24 03:29:22 +0900

    ✨  Data [ crypto ] : 토이프로젝트 크립토웹 업로드

--- src/data/toyData.ts
+++ src/data/toyData.ts
@@ -2,36 +2,36 @@ import { project } from "./projectData";
 
 const toyData: project[] = [
   {
-    id: 20210101,
-    title: "칼하트클론WIP 클론",
-    desc: "Sass로 webgird를 사용해 구현",
+    id: 20210102,
+    title: "칼하트 WIP사이트를 sass, grid를 사용해 구현했습니다",
+    desc: "Sass, Grid 구현",
     keywords: ['jquery','sass'],
     thumbnail: "./images/toy/calhartt.png",
     url: ["https://minwoobabu.cafe24.com/asset/14_project/index.html"],
     github: "https://github.com/Sunset-Kim/SASS_Carhartt_clone"
   },
   {
-    id: 20210102,
+    id: 20210101,
     title: "템플릿디자인 구현",
-    desc: "고정형 템플릿 디자인을 반응형 웹으로 개선",
+    desc: "고정형 시안을 반응형으로 개선했습니다",
     keywords: ['jquery','sass'],
     thumbnail: "./images/toy/template.png",
     url: ["https://minwoobabu.cafe24.com/asset/11_project/index.html",],
     github: "https://github.com/Sunset-Kim/Vue_templeate"
   },
   {
-    id: 20210103,
+    id: 20210104,
     title: "라이카 반응형",
-    desc: "고정형 사이트를 반응형으로 개선",
+    desc: "고정형 사이트를 반응형으로 개선했습니다",
     keywords: ['jquery','css'],
     thumbnail: "./images/toy/leica.png",
-    url: ["http://minwoobabu.cafe24.com/asset/11_project/index.html",],
+    url: ["https://minwoobabu.cafe24.com/asset/01_project/index.html",],
     github: "https://github.com/Sunset-Kim/leica-clone"
   },
   {
-    id: 20210104,
+    id: 20210103,
     title: "원페이지 스크롤 사이트",
-    desc: "간단한 형식의 원페이지 스크롤 구현",
+    desc: "간단한 형식의 원페이지 ui형식의 사이트를 만들었습니다",
     keywords: ['jquery','css'],
     thumbnail: "./images/toy/onpage.png",
     url: ["https://minwoobabu.cafe24.com/asset/02_project/index.html",],
@@ -39,29 +39,38 @@ const toyData: project[] = [
   {
     id: 20210105,
     title: "아이패드 모션페이지 제작",
-    desc: "아이패드 모션페이지와 유사한 인터렉션을 제작",
+    desc: "canvas와 vanila js를 사용해서 애플인터렉션을 구현했습니다",
     keywords: ['javascript','css'],
     video: './images/toy/jsinteraction.mp4',
     url: ["https://minwoobabu.cafe24.com/asset/13_project/index.html",],
     github: 'https://github.com/Sunset-Kim/JS_interaction_page'
   },
   {
-    id: 20210106,
+    id: 20211206,
     title: "Koovie",
-    desc: "react-native, movie API를 사용한 영화소개 app제작",
+    desc: "react-native, movie API를 사용한 영화소개 app 입니다",
     keywords: ['react-native','typescript'],
     video: './images/toy/koovie.mp4',
     url: [],
     github: 'https://github.com/Sunset-Kim/Koovies'
   },
   {
-    id: 20210107,
+    id: 20211207,
     title: "Kimflix",
-    desc: "react, movie API를 사용한 영화소개 web제작",
-    keywords: ['react-native','typescript'],
+    desc: "react, movie API, swipe를 사용한 영화소개페이지 입니다",
+    keywords: ['react','typescript'],
     thumbnail: './images/toy/kimflix.png',
     url: ['https://kimflix11.netlify.app/'],
     github: 'https://github.com/Sunset-Kim/Koovies'
+  },
+  {
+    id: 20211224,
+    title: "Crypto Web",
+    desc: "react, react-query,chart 라이브러리, CoinApi으로 만든 코인 정보 사이트 입니다",
+    keywords: ['react','typescript'],
+    thumbnail: './images/toy/crypto.png',
+    url: ['https://sunset-kim.github.io/crypto-app/'],
+    github: 'https://github.com/Sunset-Kim/crypto-app'
   }
 ];
 
